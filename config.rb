1a3c63dbb42c770ae6efe5c3b9bb8a36f5ab61b6
Naomi Guyer <adrexia@gmail.com>
2012-07-23 13:11:08 +1200

    MINOR: Comment config.rb

--- config.rb
+++ config.rb
@@ -1,9 +1,23 @@
+# Config file for compass. 
+# Run "compass watch" on the dir this is in to compile you scss 
+# i.e On a mac: 
+# Open terminal, 
+# cd to your projects themes folder (cd sites/ssexpress/themes/ssexpress)
+# type: compass watch
+
 project_type = :stand_alone
+# Set this to the root of your project when deployed:
 http_path = "/"
 css_dir = "css"
 sass_dir = "scss"
 images_dir = "images"
-javascripts_dir = "javascript"
+javascripts_dir = "js"
+
+#To compile css in less readable format, change this to :compact
 output_style = :expanded
+
+# To enable relative paths to assets via compass helper functions. Uncomment:
 relative_assets = true
+
+# disable comments in the output
 line_comments = false
