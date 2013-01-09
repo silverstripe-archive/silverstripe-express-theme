c7cbb35b454570d720cfd0fa5ee6a6b489e3c3bc
Naomi Guyer <naomi@silverstripe.com>
2013-01-10 12:53:34 +1300

    Javascript clean up

--- js/forms.js
+++ js/forms.js
@@ -1,14 +1,13 @@
-(function($) {
-	$(document).ready(function() {
+/*jslint browser: true*/
+/*global $, jQuery*/
 
+$(function() {
+	"use strict";
 
-		$( "input.date" ).datepicker({
-			showOn: "button",
-			buttonImage: "images/icons/sitetree_images/date.png",
-			buttonImageOnly: true
-		});
+	$( "input.date" ).datepicker({
+		showOn: "button",
+		buttonImage: "images/icons/sitetree_images/date.png",
+		buttonImageOnly: true
+	});
 	
-
-		
-	})
-})(jQuery);
+});
